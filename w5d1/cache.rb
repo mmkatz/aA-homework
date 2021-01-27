a3b3ef91767dc2a341913025071da1e5e8adfa3e
mmk <menachem3093@gmail.com>
2021-01-27 09:19:11 -0500

    w5d3

---
+++ w5d1/cache.rb
@@ -0,0 +1,35 @@
+ class LRUCache
+    def initialize(size)
+        @cache = []
+        @size = size 
+
+    end
+
+    def count
+        @cache.length 
+      # returns number of elements currently in cache
+    end
+
+    def add(el)
+        if @cache.include?(el)
+            @cache.delete(el)
+            @cache << el 
+        elsif 
+            @cache.length >= @size 
+            @cache.shift 
+            @cache << el 
+        else 
+            @cache << el 
+        end
+      # adds element to cache according to LRU principle
+    end
+
+    def show
+        p @cache 
+      # shows the items in the cache, with the LRU item first
+    end
+
+    private
+    # helper methods go here!
+
+  end
